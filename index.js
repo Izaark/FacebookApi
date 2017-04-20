398c562746c7111e37b28862fdc7b539ecb30371
Isaac López <izaak00@hotmail.com>
2017-04-19 22:29:14 -0500

    Post % Friends

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 var passport = require("passport");
 var cookieSession = require("cookie-session");
 var Strategy = require("passport-facebook").Strategy;
-var grap = require("fbgraph");
+var graph = require("fbgraph");
 var User = require('./models/user');
 var app = express();
 
@@ -26,20 +26,23 @@ passport.use(new Strategy({
 	clientID: '398599473860653',
 	clientSecret: 'bff1e13cb4471f60ba85c991c253cb8e',
 	callbackURL: 'http://localhost:8000/auth/facebook/callback'
-	},function(accesToken,refreshToken, profile, cb){
+	},function(accessToken,refreshToken, profile, cb){
 
 		//Save user
 
 		User.findOrCreate({uid: profile.id},
-			{name: profile.displayName, provider:"facebook",
-			accesToken: accesToken
-		},function(err, user){
+		{name: profile.displayName, provider:"facebook"},function(err,user){
 
-			cb(null, user);
+				var userSession = {
+					accessToken: accessToken,
+					profile: user
+				}
+
+				return cb(null,userSession);
+
 		});
-	}
 
-));
+}));
 
 
 passport.serializeUser(function(user,done){
@@ -53,12 +56,11 @@ passport.deserializeUser(function(user,done){
 //Define el flujo de auth
 
 //inicia auth y redirige a facebook
-app.get('/auth/facebook',
-	passport.authenticate('facebook',{}));
+app.get('/auth/facebook',passport.authenticate('facebook',{scope: ['publish_actions','user_friends']}));
+	
 
 //recive la respuesta de facebook a la ruta de nuestra pagina
-app.get('/auth/facebook/callback',
-	passport.authenticate('facebook',{failureRedirect: '/'}),
+app.get('/auth/facebook/callback',passport.authenticate('facebook',{failureRedirect: '/'}),
 	function(req,res){
 		console.log(req.session);
 		res.redirect('/');
@@ -76,6 +78,47 @@ app.get('/',function(req, res){
 	else{
 		res.render('home')
 	}
+});
+
+
+//Publicar
+app.post("/logros",function(req,res){
+	var publicacion = { message: req.body.logro };
+
+	graph.setAccessToken(req.session.passport.user.accessToken);
+
+	// Enviarselo a Facebook
+	graph.post("/feed",publicacion,function(err,graphResponse){
+		console.log(graphResponse);
+		res.redirect("/");
+	});
+
+});
+
+//Amigos
+
+app.get("/friends",function(req, res){
+		graph.setAccessToken(req.session.passport.user.accessToken);
+		console.log("Tus Amigos !!")
+		graph.get("/me/friends",function(err, graphResponse){
+			//res.json(graphResponse);
+
+			var ids = graphResponse.data.map(function(aux){
+				return aux.id;
+			});
+//Busca en la collecin de usuarios de mongo mediante los ids que nos da facebook en el json y compara con el uid que guardamos en mongodb
+			User.find({
+				'uid':{
+					$in: ids
+				}	
+			},function(err, users){
+				//Usuarios que se encontraron
+
+				res.render('friends',{users:users})
+				console.log(users);
+			});
+		});
+
 });
 
 app.listen(8000,function(){
